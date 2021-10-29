ff51259739125b1cfb623cd0af092c1763d7a70b
Sandhra Prathap <63464060+SandhraPrathap@users.noreply.github.com>
2021-10-29 23:12:22 +0530

    updated http package

--- lib/deta_pack.dart
+++ lib/deta_pack.dart
@@ -5,19 +5,19 @@ import 'package:http/http.dart' as http;
 class DetaBase {
   String projectId;
   String apiKey;
-  String basicUrl;
+  Uri basicUrl;
   Map<String, String> headers;
   DetaBase(String key, String projectID) {
     this.projectId = projectID;
     this.apiKey = key;
-    basicUrl = "https://database.deta.sh/v1/" + this.projectId;
+    basicUrl = Uri.parse("https://database.deta.sh/v1/") + this.projectId;
     headers = {"content-type": "application/json", "x-api-key": apiKey};
   }
 
   /// This method lets you get data from your base.
   getData(String base, String key) async {
     String url = basicUrl + '/' + base + '/items/' + key;
-    return http.get(url, headers: headers);
+    return http.get(Uri.parse(url), headers: headers);
   }
 
   /// This method let's you put new data items to your base. The 'items' argument is a string in JSON format.
@@ -25,21 +25,21 @@ class DetaBase {
   putData(String base, String items) async {
     String url = basicUrl + '/' + base + '/items';
     String body = '{"items":[$items]}';
-    return http.put(url, headers: headers, body: body);
+    return http.put(Uri.parse(url), headers: headers, body: body);
   }
 
   /// This method is for deleting data objects from the base.
   /// This method takes two arguments, the base name and the key of the item to be deleted.
   deleteData(String base, String key) async {
     String url = '$basicUrl/$base/items/$key';
-    return http.delete(url, headers: headers);
+    return http.delete(Uri.parse(url), headers: headers);
   }
 
   /// This method adds an item to the base if the key does not already exist, i.e. creates a new item only if an item with the same key does not already exist.
   /// This method takes in two arguments, baseName and the item to be inserted. Only single items can be inserted.
   insertData(String base, String item) async {
     String url = '$basicUrl/$base/items';
-    return http.post(url, headers: headers, body: item);
+    return http.post(Uri.parse(url), headers: headers, body: item);
   }
 
   /// This method lets you update specific fields of an existing item and also delete specific fields from the item.
@@ -47,7 +47,7 @@ class DetaBase {
   updateData(String base, String key, String update, String delete) {
     String url = '$basicUrl/$base/items/$key';
     String body = '"set":$update,"delete":[$delete]';
-    return http.patch(url, headers: headers, body: body);
+    return http.patch(Uri.parse(url), headers: headers, body: body);
   }
 
   /// This method lets you query data from the base. i.e. gets you data that matches given queries.
@@ -58,6 +58,6 @@ class DetaBase {
     String body = '"query":[$queries]';
     body += (limit != null) ? ',"limit":$limit' : '';
     body += (limit != null) ? ',"last":"$last"' : '';
-    return http.post(url, headers: headers, body: body);
+    return http.post(Uri.parse(url), headers: headers, body: body);
   }
 }
